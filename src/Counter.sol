394e7fcec6197854bb671eee908dc4ebf3a055d3
uintgroup <logins@uint.sh>
2023-08-09 13:30:55 -0400

    init-idea

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {SelectivePause} from "./SelectivePause.sol";
+
 contract Counter {
     uint256 public number;
 
-    function setNumber(uint256 newNumber) public {
+    function setNumber(uint256 newNumber) public whenNotPaused(false) {
         number = newNumber;
     }
 
-    function increment() public {
+    function increment() public whenNotPaused(true) {
         number++;
     }
 }
---
+++ src/SelectivePause.sol
@@ -0,0 +1,56 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+/**
+ * @title SelectivePause
+ * @author @uintgroup @curi0n-s
+ * @notice pauses contract functions using each function's selector
+ * @dev replaces OpenZeppelin's Pausable.sol (whenNotPaused would conflict)
+ */
+
+abstract contract SelectivePause {
+    // @notice revert with this custom error when array lengths do not match
+    error ArrayLengthMismatch();
+
+    // @notice revert with this custom error when a function is called while paused
+    error FunctionIsPaused();
+
+    // @notice emitted when a functions pause state is changed
+    event FunctionIsPausedUpdate(
+        bytes4 indexed functionSelector,
+        bool isPaused
+    );
+
+    /// @notice function is paused by admins: functionId => bool
+    mapping(bytes4 => bool) public functionIsPaused;
+
+    /// @dev reverts if the function of the function selector in msg.sig is paused
+    modifier whenNotPaused(bool _pauseAfterCall) {
+        if (functionIsPaused[msg.sig]) {
+            revert FunctionIsPaused();
+        }
+        _;
+        functionIsPaused[msg.sig] = _pauseAfterCall;
+    }
+
+    /// @dev modifies pause state of single function
+    function setIsPaused(bytes4 _functionSelector, bool _isPaused) external {
+        functionIsPaused[_functionSelector] = _isPaused;
+        emit FunctionIsPausedUpdate(_functionSelector, _isPaused);
+    }
+
+    /// @dev modifies pause state of multiple functions
+    function batchSetFunctionIsPaused(
+        bytes4[] calldata _selectors,
+        bool[] calldata _isPaused
+    ) external {
+        if (_selectors.length != _isPaused.length) {
+            revert ArrayLengthMismatch();
+        }
+
+        for (uint256 i = 0; i < _selectors.length; i++) {
+            functionIsPaused[_selectors[i]] = _isPaused[i];
+            emit FunctionIsPausedUpdate(_selectors[i], _isPaused[i]);
+        }
+    }
+}
